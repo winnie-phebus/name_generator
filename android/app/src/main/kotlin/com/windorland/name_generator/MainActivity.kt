a17b0a6d93e9426ac552c52047b1606be6caa59d
winnie.phebus <winn.phebus@gmail.com>
2020-11-12 13:43:55 -0500

    init

---
+++ android/app/src/main/kotlin/com/windorland/name_generator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.windorland.name_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/names.dart
@@ -0,0 +1,21 @@
+import 'package:name_generator/services/networking.dart';
+
+const btnApiKey = 'wi948351677';
+const JSONbtnUrl = 'https://www.behindthename.com/api/random.json';
+
+class NameRetriever {
+  Future<dynamic> getRandomNames(
+      String gender, String usage, int number, bool surname) async {
+    String surnameString = surname ? 'yes' : 'no';
+    String url = JSONbtnUrl +
+        '?usage=$usage&gender=$gender&number=$number&key=$btnApiKey';
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    print('returning from NameRetreiver');
+    return await networkHelper.getData();
+  }
+
+  Future<dynamic> getLastName(String usage) async {
+    return await getRandomNames('', usage, 1, true);
+  }
+}
